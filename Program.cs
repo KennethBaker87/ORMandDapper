58ad8e4c4ff3c060f35d33a719032774ebb8a0f3
Kenneth Baker <spyksaturn@gmail.com>
2023-03-03 11:12:54 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ORMandDapper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
